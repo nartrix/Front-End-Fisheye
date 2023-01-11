607cd311b94307c227bc5b6afce053a7498b68a8
nartrix <dimitri.danielo@laposte.net>
2023-01-11 23:33:36 +0100

    Feat: build function in photographer page

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,63 +1,33 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-undef */
-/* eslint-disable consistent-return */
-// eslint-disable-next-line no-use-before-define
-getPhotographers();
+import { photographerFactory, getUserCardDOM } from "../factories/photographer.js";
 
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  /* let photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]}) 
-    */
-
-  const API_URL = "../../data/photographers.json"; // Récupération du fichier comportant les données
-
-  await fetch(`${API_URL}`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      const { photographers } = data; // récupération des données du tableau photographers
-      console.log(photographers);
-      // eslint-disable-next-line no-use-before-define
-      displayData(photographers); // affichage des données
-    })
-    .catch(() => {
-      // retour erreur
-    });
+  return fetch("./data/photographers.json")
+      .then((res) => {
+          if (res.ok) {
+              return res.json();
+          }
+      })
+      .then((data) => data)
+      .catch((err) => {
+          alert(`Erreur : ${err}`);
+      });
 }
 
 async function displayData(photographers) {
   const photographersSection = document.querySelector(".photographer_section");
-
-  await photographers.forEach((photographer) => {
-    console.log(photographer);
+  // Récupération de chaques données contenu dans photgraphers
+  photographers.forEach((photographer) => {
     const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
+    const userCardDOM = getUserCardDOM(photographerModel);
     photographersSection.appendChild(userCardDOM);
   });
 }
+
+async function init() {
+  // Récupération des données de photogrpahers
+  const responsePhotographers = await getPhotographers();
+  console.log(responsePhotographers.photographers);
+  displayData(responsePhotographers.photographers);
+}
+
+init();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,2 +1,38 @@
-/* eslint-disable prettier/prettier */
+import { photographerFactory, getPhotographerInfos } from "../factories/photographer.js";
+
 // Mettre le code JavaScript lié à la page photographer.html.
+const photographerUrl = window.location.search;
+const urlParams = new URLSearchParams(photographerUrl);
+const photographerId = urlParams.get("id");
+
+// Appel fichier JSON
+async function getPhotographers() {
+    return fetch("./data/photographers.json")
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then((data) => data)
+        .catch((err) => {
+            alert(`Erreur : ${  err}`);
+        });
+}
+
+async function displayData(photographers) {
+    // Récupération des données du photographe selon son id
+    const currentPhotographer = photographers.find(id => id.id === parseInt(photographerId, 10));
+    console.log(currentPhotographer);
+    const photographerModel = photographerFactory(currentPhotographer);
+    getPhotographerInfos(photographerModel);
+}
+
+async function init() {
+    // Récupération des données de photogrpahers et media
+    const { photographers, media } = await getPhotographers(); 
+    const mediasOfPhotographer = media.filter(dataMedia => dataMedia.photographerId === parseInt(photographerId, 10));
+    console.log(mediasOfPhotographer);
+    displayData(photographers);
+}
+
+init();
